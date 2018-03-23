f9cb95cbef84fde1ab250f884b5690b09ae78293
André Vieira da Silva <sgavsnake@gmail.com>
2018-03-22 23:48:39 -0300

    Initial MVC

---
+++ MVC_Cursos/MVC_Cursos/IGreeter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MVC_Cursos
+{
+    public interface IGreeter
+    {
+        string GetMessageOfDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetMessageOfDay()
+        {
+            return "The Greeting of Method 'GetMessageOfDay() in class Greetings'";
+        }
+
+
+    }
+}
